8f9a57c680494027121f793b68d904a9408b10e2
a.papikyan <a.papikyan@corp.mail.ru>
2019-08-31 13:40:43 +0400

    created  Options model

---
+++ common/models/Options.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "options".
+ *
+ * @property int $id
+ * @property string $key
+ * @property string $value
+ */
+class Options extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'options';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['key'], 'required'],
+            [['value'], 'string'],
+            [['key'], 'string', 'max' => 500],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'key' => 'Key',
+            'value' => 'Value',
+        ];
+    }
+}
