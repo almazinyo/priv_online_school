0fe9d3a8bdeaf9149a2ba1643095eec5b8c46c3e
a.papikyan <a.papikyan@corp.mail.ru>
2019-09-02 02:27:46 +0400

    created  migrate  lessons

---
+++ console/migrations/m190901_212843_lessons.php
@@ -0,0 +1,67 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190901_212843_lessons
+ */
+class m190901_212843_lessons extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190901_212843_lessons cannot be reverted.\n";
+
+        return false;
+    }
+
+    public function up()
+    {
+        $this->createTable(
+            'lessons',
+            [
+                'id' => $this->primaryKey(),
+                'sort_lessons' => $this->integer(11)->defaultValue(0),
+                'title' => $this->string(500)->notNull(),
+                'section_id' => $this->integer(11),
+                'slug' => $this->string(500)->notNull(),
+                'file_path' => $this->string(500),
+                'url_video' => $this->string(500),
+                'short_description' => $this->text(),
+                'description' => $this->text(),
+                'seo_keywords' => $this->string(300),
+                'seo_description' => $this->string(300),
+                'created_at' => $this->string(300),
+                'updated_at' => $this->string(300),
+                'bonus_points' => $this->tinyInteger(2)->defaultValue(0),
+                'is_status' => $this->tinyInteger(2),
+            ],
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8'
+        );
+
+        $this->addForeignKey(
+            'FK_lessons',
+            'lessons',
+            'section_id',
+            'section_subjects',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('lessons');
+    }
+}
