8ef1c7b8120bafd3851ab647a4e87504d2299711
a.papikyan <a.papikyan@corp.mail.ru>
2019-09-03 15:27:27 +0400

    created  storage_lessons  migrate

---
+++ console/migrations/m190903_110452_storage_lessons.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190903_110452_storage_lessons
+ */
+class m190903_110452_storage_lessons extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190903_110452_storage_lessons cannot be reverted.\n";
+
+        return false;
+    }
+
+    public function up()
+    {
+        $this->createTable(
+            'storage_lessons',
+            [
+                'id' => $this->primaryKey(),
+                'lesson_id' => $this->integer(11),
+                'name' => $this->string(500)->notNull(),
+                'type' => 'ENUM("pdf","image","video")',
+                'is_status' => $this->tinyInteger(2),
+            ],
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8'
+        );
+
+        $this->addForeignKey(
+            'FK_storage',
+            'storage_lessons',
+            'lesson_id',
+            'lessons',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('storage_lessons');
+    }
+}
