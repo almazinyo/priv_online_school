d74dbef4e11ba46dc8b89a92f2d5cdb33490919f
a.papikyan <a.papikyan@corp.mail.ru>
2019-09-03 17:54:33 +0400

    CRUD menu

---
+++ backend/views/menu/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Menu */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="menu-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'logo')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'parent_id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'created_at')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'updated_at')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'is_status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
