a78f137214707fa3dee2cc405962488df262cec9
a.papikyan <a.papikyan@corp.mail.ru>
2019-09-10 11:43:01 +0400

    CRUD Teachers

---
+++ backend/views/teachers/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Teachers */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="teachers-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'position')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'section_id')->textInput() ?>
+
+    <?= $form->field($model, 'img_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'created_at')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'updated_at')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'is_status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/teachers/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\TeachersControl */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Teachers';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="teachers-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Teachers', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'position',
+            'section_id',
+            'img_name',
+            //'description:ntext',
+            //'created_at',
+            //'updated_at',
+            //'is_status',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
