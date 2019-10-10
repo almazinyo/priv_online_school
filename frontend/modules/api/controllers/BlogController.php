ae46baac4f0a1436ffdb01cdd90032e53b69ab78
a.papikyan <a.papikyan@corp.mail.ru>
2019-10-10 17:12:03 +0400

    fix  CROS

--- frontend/modules/api/controllers/BlogController.php
+++ frontend/modules/api/controllers/BlogController.php
@@ -4,6 +4,9 @@ namespace frontend\modules\api\controllers;
 
 use common\models\Blog;
 use frontend\modules\api\components\Select;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\ContentNegotiator;
+use yii\filters\Cors;
 use yii\helpers\Html;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -11,6 +14,39 @@ use yii\web\Response;
 
 class BlogController extends Controller
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function behaviors()
+    {
+        return \yii\helpers\ArrayHelper::merge([
+            [
+                'class' => \yii\filters\Cors::className(),
+                'cors' =>
+                    [
+                        'Origin' => ['*'],
+                        'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                        'Access-Control-Request-Headers' => [
+                            'Origin',
+                            'X-Requested-With',
+                            'Content-Type',
+                            'accept',
+                            /*'Authorization'*/
+                        ],
+                        'Access-Control-Expose-Headers' =>
+                            [
+                                'X-Pagination-Per-Page',
+                                'X-Pagination-Total-Count',
+                                'X-Pagination-Current-Page',
+                                'X-Pagination-Page-Count',
+                            ],
+                    ],
+            ],
+        ],
+            parent::behaviors()
+        );
+    }
+
     public function init()
     {
         \Yii::$app->response->format = Response::FORMAT_JSON;
