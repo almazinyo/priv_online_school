905a09fe6e69809cdd0759a345b9c6ba73e3b519
a.papikyan <a.papikyan@corp.mail.ru>
2019-11-08 04:04:56 +0400

    fix

--- common/config/main.php
+++ common/config/main.php
@@ -37,11 +37,11 @@ return [
                     'clientId' => 'facebook_client_id',
                     'clientSecret' => 'facebook_client_secret',
                 ],
-                'instagram' => [
-                    'class' => 'kotchuprik\authclient\Instagram',
-                    'clientId' => '7200615',
-                    'clientSecret' => 'instagram_client_secret',
-                ],
+//                'instagram' => [
+//                    'class' => 'kotchuprik\authclient\Instagram',
+//                    'clientId' => '7200615',
+//                    'clientSecret' => 'instagram_client_secret',
+//                ],
 
             ],
         ],
