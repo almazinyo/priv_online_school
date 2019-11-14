8d32ba15f38ecfb4ff8414e395ae7a85bd50eaaf
a.papikyan <a.papikyan@corp.mail.ru>
2019-11-14 17:08:19 +0400

    fix

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -58,8 +58,8 @@ return [
 //        ],
         'user' => [
             'identityClass' => 'frontend\models\User',
-            'enableAutoLogin' => false,
-            'enableSession' => false,
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
         ],
         'session' => [
             // this is the name of the session cookie used for login on the frontend
