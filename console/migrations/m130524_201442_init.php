cd7daa700bace8bf82a131b649163fe629043f95
a.papikyan <a.papikyan@corp.mail.ru>
2019-11-20 17:32:46 +0400

    change usaer migrate

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -12,27 +12,68 @@ class m130524_201442_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%user}}', [
-            'id' => $this->primaryKey(),
-            'username' => $this->string()->notNull()->unique(),
-            'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string()->notNull(),
-            'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull()->unique(),
+        $this->createTable(
+            '{{%user}}',
+            [
+                'id' => $this->primaryKey(),
+                'username' => $this->string()->notNull()->unique(),
+                'auth_key' => $this->string(32)->notNull(),
+                'password_hash' => $this->string()->notNull(),
+                'password_reset_token' => $this->string()->unique(),
+                'email' => $this->string()->notNull()->unique(),
 
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
-        ], $tableOptions);
+                'status' => $this->smallInteger()->notNull()->defaultValue(10),
+                'created_at' => $this->integer()->notNull(),
+                'updated_at' => $this->integer()->notNull(),
+            ],
+            $tableOptions
+        );
 
-        $this->createTable('auth', [
-            'id' => $this->primaryKey(),
-            'user_id' => $this->integer()->notNull(),
-            'source' => $this->string()->notNull(),
-            'source_id' => $this->string()->notNull(),
-        ]);
+        $user = new \common\models\User();
+        $user->username = 'admin';
+        $user->email = 'admin@example.com';
+        $user->generateAuthKey();
+        $user->setPassword('demo');
+        $user->save(false);
 
-        $this->addForeignKey('fk-auth-user_id-user-id', 'auth', 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+        $this->createTable(
+            'session',
+            [
+                'id' => $this->char(40)->notNull(),
+                'expire' => $this->integer(),
+                'data' => $this->binary(),
+                'user_id' => $this->integer(),
+            ]
+        );
+
+        $this->addForeignKey(
+            'fk-session-user_id-user-id',
+            'session',
+            'user_id',
+            'user',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->createTable(
+            'auth',
+            [
+                'id' => $this->primaryKey(),
+                'user_id' => $this->integer()->notNull(),
+                'source' => $this->string()->notNull(),
+                'source_id' => $this->string()->notNull(),
+            ]);
+
+        $this->addForeignKey(
+            'fk-auth-user_id-user-id',
+            'auth',
+            'user_id',
+            'user',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
     }
 
     public function down()
