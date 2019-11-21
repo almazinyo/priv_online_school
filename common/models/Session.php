572c6c414b739a28038e271953821a6030b92f87
a.papikyan <a.papikyan@corp.mail.ru>
2019-11-21 10:03:15 +0400

    fix

---
+++ common/models/Session.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "session".
+ *
+ * @property string $id
+ * @property int|null $expire
+ * @property resource|null $data
+ * @property string|null $token
+ * @property int|null $user_id
+ * @property int|null $status
+ *
+ * @property User $user
+ */
+class Session extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'session';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['expire', 'user_id', 'status'], 'integer'],
+            [['data'], 'string'],
+            [['id'], 'string', 'max' => 40],
+            [['token'], 'string', 'max' => 500],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'expire' => Yii::t('app', 'Expire'),
+            'data' => Yii::t('app', 'Data'),
+            'token' => Yii::t('app', 'Token'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'status' => Yii::t('app', 'Status'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
