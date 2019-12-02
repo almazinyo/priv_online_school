aecf369fd8f42fbf2fd25c7f5c77b9aafed9de38
a.papikyan <a.papikyan@corp.mail.ru>
2019-12-02 15:02:51 +0400

    hotFix:  cors in users

--- frontend/modules/api/controllers/UsersController.php
+++ frontend/modules/api/controllers/UsersController.php
@@ -13,6 +13,39 @@ use yii;
 class UsersController extends Controller
 {
     /**
+     * {@inheritDoc}
+     */
+    public function behaviors()
+    {
+        return \yii\helpers\ArrayHelper::merge([
+            [
+                'class' => \yii\filters\Cors::className(),
+                'cors' =>
+                    [
+                        'Origin' => ['*'],
+                        'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                        'Access-Control-Request-Headers' => [
+                            'Origin',
+                            'X-Requested-With',
+                            'Content-Type',
+                            'accept',
+                            /*'Authorization'*/
+                        ],
+                        'Access-Control-Expose-Headers' =>
+                            [
+                                'X-Pagination-Per-Page',
+                                'X-Pagination-Total-Count',
+                                'X-Pagination-Current-Page',
+                                'X-Pagination-Page-Count',
+                            ],
+                    ],
+            ],
+        ],
+            parent::behaviors()
+        );
+    }
+
+    /**
      * @var UsersService
      */
     private $userService;
